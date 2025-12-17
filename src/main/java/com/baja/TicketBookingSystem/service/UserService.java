5655723f7344f1e1abe4f974475d389798a26589
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2025-12-17 22:25:35 +0530

    Implemeted password hashing using BCrypt

--- src/main/java/com/baja/TicketBookingSystem/service/UserService.java
+++ src/main/java/com/baja/TicketBookingSystem/service/UserService.java
@@ -5,17 +5,19 @@ import com.baja.TicketBookingSystem.entity.User;
 import com.baja.TicketBookingSystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder; 
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User registerUser(UserDto userDto) {
         User user = new User();
         user.setName(userDto.getName());
         user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
         user.setPhoneNumber(userDto.getPhoneNumber());
 
         return userRepository.save(user);
